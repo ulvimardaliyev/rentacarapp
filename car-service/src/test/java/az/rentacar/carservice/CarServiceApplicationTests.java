12e9f8ccc0e543551d3ec2fb1ca53cb32c09c5de
ulvi <merdeliyev@inbox.ru>
2022-05-02 01:14:35 +0400

    Created car-service module, modified setting.gradle, groups of build.gradle, added mongodb

---
+++ car-service/src/test/java/az/rentacar/carservice/CarServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package az.rentacar.carservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CarServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
