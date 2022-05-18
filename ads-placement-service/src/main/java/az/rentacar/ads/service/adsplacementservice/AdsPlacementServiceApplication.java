2a816098786d5fe017d4c3c31eef4f9662eaafc2
ulvi <merdeliyev@inbox.ru>
2022-05-18 22:04:51 +0400

    Configured all microservices for eureka server discovery

--- ads-placement-service/src/main/java/az/rentacar/ads/service/adsplacementservice/AdsPlacementServiceApplication.java
+++ ads-placement-service/src/main/java/az/rentacar/ads/service/adsplacementservice/AdsPlacementServiceApplication.java
@@ -2,8 +2,10 @@ package az.rentacar.ads.service.adsplacementservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 @SpringBootApplication
+@EnableEurekaClient
 public class AdsPlacementServiceApplication {
 
     public static void main(String[] args) {
